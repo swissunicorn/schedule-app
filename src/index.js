9f3271f7dad0110a65bdfa1cb6256663cedbe59a
Sydney <sydney.rae.smith@gmail.com>
2022-04-08 07:41:12 -0500

    styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // imports Bootstrap default CSS
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 

d1735c1025fd40c73d2dcf42cd5436112e591d12
Sydney <sydney.rae.smith@gmail.com>
2022-04-10 22:31:49 -0500

    filtering

--- src/App.js
+++ src/App.js
@@ -52,11 +52,40 @@ const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-    { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+const TermSelector = ({term, setTerm}) => (
+  <div className = "btn-group">
+    {
+      Object.values(terms).map(value => (
+        <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+      ))
+    }  
   </div>
 );
+
+// what is htmlFor
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+      onChange={() => setTerm(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+      { term }  
+    </label>
+  </>
+)
+
+const CourseList = ({ courses }) => { // <> is a way to return two components without making a div
+  const [term, setTerm] = useState('Fall');
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+
+  return (
+    <> 
+      <TermSelector term={term} setTerm={setTerm}/>
+      <div className="course-list">
+      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      </div>
+    </>
+  )
+};
 
 const App = () => {
   const [schedule, setSchedule] = useState();
